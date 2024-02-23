4f74f99da3792c29cb2d31798148fba3c0fa7df1
Mario Ortega <mabel010830@gmail.com>
2024-02-22 18:55:11 -0600

    Proyecto APIPERSON .NET 8.0

---
+++ Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiPerson.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Age { get; set; }
+    }
+}
